bc002486dc4d4dfcb185684be80693d1ca4bf1f1
CloudAppDev <CloudAppDev@WIN-0B5HA0VMEOQ>
2016-11-09 12:12:16 +0400

    3rd commit added port

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.listen(appEnv.port, '0.0.0.0', function() {
 var http = require('http');
 
 //Lets define a port we want to listen to
-const PORT=8080; 
+const PORT=appEnv.port; 
 
 //We need a function which handles requests and send response
 function handleRequest(request, response){
@@ -45,5 +45,5 @@ var server = http.createServer(handleRequest);
 //Lets start our server
 server.listen(PORT, function(){
     //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT);
+    console.log("Server listening on port: "+PORT);
 });
